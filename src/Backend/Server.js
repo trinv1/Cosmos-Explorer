df7033bb99e16e8114696502eec7ca3e92a0793c
trinv1 <trinityvillaruel@gmail.com>
2024-12-06 18:58:19 +0000

    Deleted comments

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -101,38 +101,3 @@ app.listen(port, () => {
 
 
 
-
-/*   Name: "Milky Way Galaxy",
-            Description: "The galaxy containing the Sun and its Solar System, and therefore Earth.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/4/43/ESO-VLT-Laser-phot-33a-07.jpg"
-          },
-          {
-            Name: "Andromeda Galaxy",
-            Description: "Andromeda is the closest big galaxy to the Milky Way and is expected to collide with the Milky Way around 4.5 billion years from now. The two will eventually     merge into a single new galaxy called Milkdromeda.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/9/98/Andromeda_Galaxy_%28with_h-alpha%29.jpg"
-          },
-          {
-            Name: "Bode's Galaxy",
-            Description: "Also known as Messier 81. The largest galaxy in the M81 Group. It harbors a supermassive black                           hole 70 million times the mass of the Sun.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/5/59/Messier81.png"
-          },
-          {
-            Name: "Cartwheel Galaxy",
-            Description: "Also known as Messier 81. The largest galaxy in the M81 Group. It harbors a supermassive black hole 70 million times the mass of the Sun.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Cartwheel_Galaxy_JWST_NIRCam%2BMIRI_Full_Res.png"
-          },
-          {
-            Name: "Antennae Galaxies",
-            Description: "The Antenna Galaxies are two colliding galaxies and got their name due to the resemblance of an insects antennae",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/3/30/Antennae_Galaxies_reloaded.jpg"
-          },
-          {
-            Name: "Condor Galaxy",
-            Description: "The largest known spiral galaxy, it has a diameter of over 665,300 light-years (204.0 kiloparsecs).[3] It is tidally disturbed by the smaller lenticular galaxy IC 4970.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/6/65/NGC_6872_and_IC_4970_by_ESO_VLT.jpg"
-          },
-          {
-            Name: "Cosmos Redshift 7",
-            Type: "Galaxy",
-            Description: "Galaxy Cosmos Redshift 7 is reported to be the brightest of distant galaxies and to contain some of the earliest first stars that produced the chemical elements needed for the later formation of planets and life as we know it.",
-            Picture: "https://upload.wikimedia.org/wikipedia/commons/e/ee/Eso1524aArtist%E2%80%99s_impression_of_CR7_the_brightest_galaxy_in_the_early_Universe.jpg"*/
